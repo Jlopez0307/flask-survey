481f580a05d9e1548faf6dac0bf14ae4f3b4a148
Jlopez0307 <55158803+Jlopez0307@users.noreply.github.com>
2022-10-27 21:04:31 -0400

    added home route and created base template for the home page and the questions page. Started the questions route

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'cats'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.route('/')
+def app_home():
+    title = satisfaction_survey.title
+    instructions = satisfaction_survey.instructions
+    return render_template('home.html', survey_title = title, instructions = instructions)
+
+@app.route('/questions/<int:q_num>')
+def question(q_num):
+    question = satisfaction_survey.questions[q_num]
+    return render_template('question_0.html', q_num=q_num , question = question)
+
+@app.route('/answer', methods = ['POST'])
+def handle_answers():
+    return redirect(f"/questions/{q_num}")
+
