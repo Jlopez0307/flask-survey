5c8fecdac10692940f8d37fa0afe482684479ee4
Jlopez0307 <55158803+Jlopez0307@users.noreply.github.com>
2022-10-31 10:28:43 -0400

    added logic to prevent user from skipping questions. Added thank you page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -17,10 +17,28 @@ def app_home():
 
 @app.route('/questions/<int:q_num>')
 def question(q_num):
+    res = responses
+    if len(res) != q_num:
+        flash('Please answer questions in order')
+        return redirect(f"/questions/{len(res)}")
+
+    if len(res) == len(satisfaction_survey.questions):
+        return redirect('/survey_done')
+    
+    q_choices = satisfaction_survey.questions[q_num].choices
+
+
     question = satisfaction_survey.questions[q_num]
-    return render_template('question_0.html', q_num=q_num , question = question)
+    return render_template('question_0.html', q_num=q_num , question = question, q_choices = q_choices)
 
 @app.route('/answer', methods = ['POST'])
 def handle_answers():
-    return redirect(f"/questions/{q_num}")
+    res = request.form['answer']
+    responses.append(res)
+    app.logger.info(responses)
+    return redirect(f"/questions/{len(responses)}")
+
+@app.route('/survey_done')
+def render_done():
+    return render_template('survey_done.html')
 
