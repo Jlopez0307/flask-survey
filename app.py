79c5a892916ebce504a77a2aee9d33f6b384bb98
Jlopez0307 <55158803+Jlopez0307@users.noreply.github.com>
2022-11-02 09:04:29 -0400

    added session to app. Survey can be restarted from home page and still protects questions from being jumped around

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, flash
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -7,17 +7,17 @@ app.config['SECRET_KEY'] = 'cats'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-responses = []
 
 @app.route('/')
 def app_home():
+    session["responses"] = []
     title = satisfaction_survey.title
     instructions = satisfaction_survey.instructions
     return render_template('home.html', survey_title = title, instructions = instructions)
 
 @app.route('/questions/<int:q_num>')
 def question(q_num):
-    res = responses
+    res = session["responses"]
     if len(res) != q_num:
         flash('Please answer questions in order')
         return redirect(f"/questions/{len(res)}")
@@ -29,13 +29,14 @@ def question(q_num):
 
 
     question = satisfaction_survey.questions[q_num]
-    return render_template('question_0.html', q_num=q_num , question = question, q_choices = q_choices)
+    return render_template('question_0.html', q_num = q_num , question = question, q_choices = q_choices)
 
 @app.route('/answer', methods = ['POST'])
 def handle_answers():
+    responses = session["responses"]
     res = request.form['answer']
     responses.append(res)
-    app.logger.info(responses)
+    session["responses"] = responses
     return redirect(f"/questions/{len(responses)}")
 
 @app.route('/survey_done')
